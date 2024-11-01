beaa4462b72a52da7860f204e2d6623cacb5efa3
HuyLuong <luonghuy131001@gmail.com>
2024-11-01 10:33:04 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,25 @@
 const TodoNew = (props) => {
   console.log("check point: ", props);
   const { addNewToDo } = props;
-//   addNewToDo("Huy");
+  //   addNewToDo("Huy");
+  const handleClick = () => {
+    alert("Click me");
+  };
+
+  const handleOnChange = (name) => {
+    console.log("handleOnChange", name);
+  };
   return (
     <div className="todo-new">
-      <input type="text" />
-      <button>Add</button>
+      <input
+        type="text"
+        onChange={(event) => {
+          handleOnChange(event.target.value);
+        }}
+      />
+      <button style={{ cursor: "pointer" }} onClick={handleClick}>
+        Add
+      </button>
     </div>
   );
 };
