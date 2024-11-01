c780f334a8d47856d33dbef4ff645e9e250764fa
HuyLuong <luonghuy131001@gmail.com>
2024-11-01 09:53:54 +0700

    video 34

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,9 +4,19 @@
 import "./style.css";
 
 const MyComponent = () => {
+  // const hoidanit = 25; //String
+  // const hoidanit = true; //boolean
+  // const hoidanit = undefined;
+  // const hoidanit = null;
+  // const hoidanit = [1, 2, 3];
+  const hoidanit = {
+    name: "hoidanit",
+    age: 25,
+  };
   return (
     <>
-      <div>Huy va Cati Update</div>
+      <div> {JSON.stringify(hoidanit)} Huy va Cati Update</div>
+      <div>{console.log("ERIC")}</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         child
       </div>
