40085072ed1b584a391531b49e9c38bfd1685129
HuyLuong <luonghuy131001@gmail.com>
2024-11-01 08:26:02 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,17 @@
 // component = html + css + js
+// Fragment <> </>
+
+import "./style.css";
+
 const MyComponent = () => {
-  return <div>Huy va Cati Update</div>;
+  return (
+    <>
+      <div>Huy va Cati Update</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        child
+      </div>
+    </>
+  );
 };
 
 export default MyComponent;
